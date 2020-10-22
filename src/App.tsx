9ed33c21676cc6b6465c62b0c2ef86a57de389c3
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-10-22 13:53:50 -0300

    Initial strucuture

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, Álvaro Bianor!</Text>
+    </View>
+  );
+};
+export default HelloWorldApp;
