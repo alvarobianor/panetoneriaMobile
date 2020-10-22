c74038ad562e07fc824fe71b655748301e00b0e7
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-10-22 18:05:59 -0300

    Routes finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+
+import Routes from './routes/index';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Ola</Text>
-      </View>
+      <Routes />
     </NavigationContainer>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-// import { Container } from './styles';
+import Dashboard from '../pages/Dashboard/index';
+import Details from '../pages/Details/index';
+
+const Stack = createStackNavigator();
 
 const routes: React.FC = () => {
-  return <View />;
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {backgroundColor: '#c5c5c5'},
+      }}>
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+  );
 };
 
 export default routes;
